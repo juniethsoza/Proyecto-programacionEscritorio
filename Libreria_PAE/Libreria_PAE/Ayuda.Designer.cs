cc29367c1110baef55e72c28dddef729a199a5ef
neymarencopaoloevan-blip <neymarencopaoloevan@gmail.com>
2026-08-24 10:03:03 -0600

    feat: Agregar ventana reporte

---
+++ Libreria_PAE/Libreria_PAE/Ayuda.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Libreria_PAE
+{
+    partial class frmAyuda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAyuda));
+            this.grpManualUso = new System.Windows.Forms.GroupBox();
+            this.lblManualUso = new System.Windows.Forms.Label();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.grpSoporteTecnico = new System.Windows.Forms.GroupBox();
+            this.lblSoporteTecnico = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.grpManualUso.SuspendLayout();
+            this.grpSoporteTecnico.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpManualUso
+            // 
+            this.grpManualUso.Controls.Add(this.lblManualUso);
+            this.grpManualUso.Controls.Add(this.btnAcceder);
+            this.grpManualUso.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpManualUso.Location = new System.Drawing.Point(35, 28);
+            this.grpManualUso.Name = "grpManualUso";
+            this.grpManualUso.Size = new System.Drawing.Size(722, 189);
+            this.grpManualUso.TabIndex = 0;
+            this.grpManualUso.TabStop = false;
+            this.grpManualUso.Text = "Manual de uso";
+            this.grpManualUso.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblManualUso
+            // 
+            this.lblManualUso.AutoSize = true;
+            this.lblManualUso.Location = new System.Drawing.Point(36, 83);
+            this.lblManualUso.Name = "lblManualUso";
+            this.lblManualUso.Size = new System.Drawing.Size(666, 20);
+            this.lblManualUso.TabIndex = 1;
+            this.lblManualUso.Text = "Consulta la documentacion del sistema para conocer el uso correcto de cada modulo" +
+    "";
+            this.lblManualUso.Click += new System.EventHandler(this.lblManualUso_Click);
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.Color.Purple;
+            this.btnAcceder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAcceder.Location = new System.Drawing.Point(613, 139);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(103, 45);
+            this.btnAcceder.TabIndex = 2;
+            this.btnAcceder.Text = "Acceder";
+            this.btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // grpSoporteTecnico
+            // 
+            this.grpSoporteTecnico.Controls.Add(this.btnCerrar);
+            this.grpSoporteTecnico.Controls.Add(this.lblSoporteTecnico);
+            this.grpSoporteTecnico.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpSoporteTecnico.Location = new System.Drawing.Point(35, 248);
+            this.grpSoporteTecnico.Name = "grpSoporteTecnico";
+            this.grpSoporteTecnico.Size = new System.Drawing.Size(722, 190);
+            this.grpSoporteTecnico.TabIndex = 3;
+            this.grpSoporteTecnico.TabStop = false;
+            this.grpSoporteTecnico.Text = "Soporte tecnico";
+            // 
+            // lblSoporteTecnico
+            // 
+            this.lblSoporteTecnico.AutoSize = true;
+            this.lblSoporteTecnico.Location = new System.Drawing.Point(6, 51);
+            this.lblSoporteTecnico.Name = "lblSoporteTecnico";
+            this.lblSoporteTecnico.Size = new System.Drawing.Size(568, 100);
+            this.lblSoporteTecnico.TabIndex = 4;
+            this.lblSoporteTecnico.Text = "Si presenta problemas con el sistema contacte con el equipo de soporte.\r\n\r\nCorreo" +
+    ": soportetecnico@gmail.com\r\n\r\nTelefono: 8888-8888";
+            this.lblSoporteTecnico.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Purple;
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCerrar.Location = new System.Drawing.Point(613, 139);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(103, 45);
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // frmAyuda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.grpSoporteTecnico);
+            this.Controls.Add(this.grpManualUso);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmAyuda";
+            this.Text = "Ayuda";
+            this.grpManualUso.ResumeLayout(false);
+            this.grpManualUso.PerformLayout();
+            this.grpSoporteTecnico.ResumeLayout(false);
+            this.grpSoporteTecnico.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpManualUso;
+        private System.Windows.Forms.Label lblManualUso;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.GroupBox grpSoporteTecnico;
+        private System.Windows.Forms.Label lblSoporteTecnico;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
--- Libreria_PAE/Libreria_PAE/Operaciones.Designer.cs
+++ Libreria_PAE/Libreria_PAE/Operaciones.Designer.cs
@@ -31,29 +31,29 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOperaciones));
             this.tbcOperaciones = new System.Windows.Forms.TabControl();
             this.tbpVentaF = new System.Windows.Forms.TabPage();
-            this.tbpCompra = new System.Windows.Forms.TabPage();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.btnBuscar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbpCompra = new System.Windows.Forms.TabPage();
             this.tbcOperaciones.SuspendLayout();
             this.tbpVentaF.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -74,22 +74,21 @@
             this.tbpVentaF.BackColor = System.Drawing.Color.Thistle;
             this.tbpVentaF.Controls.Add(this.groupBox2);
             this.tbpVentaF.Controls.Add(this.groupBox1);
-            this.tbpVentaF.Location = new System.Drawing.Point(4, 32);
+            this.tbpVentaF.Location = new System.Drawing.Point(4, 29);
             this.tbpVentaF.Name = "tbpVentaF";
             this.tbpVentaF.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpVentaF.Size = new System.Drawing.Size(977, 640);
+            this.tbpVentaF.Size = new System.Drawing.Size(977, 643);
             this.tbpVentaF.TabIndex = 0;
             this.tbpVentaF.Text = "Venta/Facturación";
             // 
-            // tbpCompra
+            // groupBox2
             // 
-            this.tbpCompra.BackColor = System.Drawing.Color.Thistle;
-            this.tbpCompra.Location = new System.Drawing.Point(4, 32);
-            this.tbpCompra.Name = "tbpCompra";
-            this.tbpCompra.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpCompra.Size = new System.Drawing.Size(977, 640);
-            this.tbpCompra.TabIndex = 1;
-            this.tbpCompra.Text = "Compra";
+            this.groupBox2.Location = new System.Drawing.Point(38, 312);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(912, 283);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
             // 
             // groupBox1
             // 
@@ -120,160 +119,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // groupBox2
+            // button2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(38, 312);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(912, 283);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 39);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Código";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 86);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 23);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Cliente";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(-4, 134);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 23);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Producto";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 195);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 23);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Precio";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(230, 195);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 23);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Subtotal";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(486, 39);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(105, 23);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "N° Factura";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(515, 83);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(63, 23);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Fecha";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(486, 142);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(92, 23);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "Cantidad";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(519, 195);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(59, 23);
-            this.label9.TabIndex = 8;
-            this.label9.Text = "Stock";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(126, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 31);
-            this.textBox1.TabIndex = 9;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(603, 134);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(283, 31);
-            this.textBox2.TabIndex = 10;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(603, 36);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(283, 31);
-            this.textBox3.TabIndex = 11;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(126, 83);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(328, 31);
-            this.textBox4.TabIndex = 12;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(113, 139);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(190, 31);
-            this.textBox5.TabIndex = 13;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(603, 187);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(283, 31);
-            this.textBox6.TabIndex = 14;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(92, 192);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(132, 31);
-            this.textBox7.TabIndex = 15;
-            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(322, 192);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(132, 31);
-            this.textBox8.TabIndex = 16;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(603, 83);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(283, 31);
-            this.comboBox1.TabIndex = 17;
+            this.button2.Location = new System.Drawing.Point(328, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 40);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // btnBuscar
             // 
@@ -284,21 +137,168 @@
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // comboBox1
             // 
-            this.button2.Location = new System.Drawing.Point(328, 128);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 40);
-            this.button2.TabIndex = 19;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(603, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(283, 28);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(322, 192);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(132, 27);
+            this.textBox8.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(92, 192);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(132, 27);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(603, 187);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(283, 27);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(113, 139);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(190, 27);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 83);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(328, 27);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(603, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(283, 27);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(603, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 27);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 27);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(519, 195);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Stock";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(486, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Cantidad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(515, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Fecha";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(486, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "N° Factura";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(230, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Subtotal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-4, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // tbpCompra
+            // 
+            this.tbpCompra.BackColor = System.Drawing.Color.Thistle;
+            this.tbpCompra.Location = new System.Drawing.Point(4, 29);
+            this.tbpCompra.Name = "tbpCompra";
+            this.tbpCompra.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpCompra.Size = new System.Drawing.Size(977, 643);
+            this.tbpCompra.TabIndex = 1;
+            this.tbpCompra.Text = "Compra";
             // 
             // frmOperaciones
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Thistle;
-            this.ClientSize = new System.Drawing.Size(979, 662);
+            this.ClientSize = new System.Drawing.Size(979, 674);
             this.Controls.Add(this.tbcOperaciones);
             this.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
